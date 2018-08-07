28941becdb0020aeab9c989b32bb2bb91d6d3f1a
rosen <30295922+rkralev@users.noreply.github.com>
2018-08-06 18:19:56 -0700

    minor fix

--- maml_examples/r7dof_vars.py
+++ maml_examples/r7dof_vars.py
@@ -32,15 +32,15 @@ EXPERT_TRAJ_LOCATION_DICT = {
     ".ec2_1000_40":      "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-individual_noise0.1/",
     ".ec2_100_40_1":     "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-100-40/", # 100 goals, 40 goals per itr, 40 et per goal
     ".ec2_200_40_1":     "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/", # 200 goals, 40 goals per itr, 40 et per goal
-    ".ec2_200_40_1dist": "/home/rosen/maml_rl/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/",
+    ".ec2_200_40_1dist": "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/",
 # 200 goals, 40 goals per itr, 40 et per goal
-    ".ec2_200_40_1_1": "/home/rosen/maml_rl/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/",
+    ".ec2_200_40_1_1": "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/",
 # 200 goals, 40 goals per itr, 40 et per goal
-    ".ec2_200_40_1dist_1": "/home/rosen/maml_rl/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/",
+    ".ec2_200_40_1dist_1": "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/",
 # 200 goals, 40 goals per itr, 40 et per goal
     ".ec2_200_40_1_5": "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40-1_5/",
 # 200 goals, 40 goals per itr, 40 et per goal
-    ".ec2_200_40_1dist_5": "/home/rosen/maml_rl/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/",
+    ".ec2_200_40_1dist_5": "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40/",
 # 200 goals, 40 goals per itr, 40 et per goal
     ".ec2_200_40_2":     "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40-2/",
     ".ec2_200_40_3":     "/root/code/rllab/saved_expert_traj/R7DOF/R7-ET-noise0.1-200-40-3/",
